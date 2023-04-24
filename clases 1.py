74cf7837b2a0d16c47982e8ab768e4d267ee3103
STEVEN <130578469+CHIVAJA@users.noreply.github.com>
2023-04-24 10:14:10 -0500

    Add files via upload

---
+++ clases 1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 24 09:34:59 2023
+
+@author: lab
+"""
+
+class persona:
+    def __init__(self,nombre,apellido,genero):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.genero=genero
+estudiante=persona("juan", "domínguez", "m")
+profesor=persona("carlos", "ayala", "m")
+
+
+
+class xd:
+    def __init__(self,nombre,apellido,lugar):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.lugar=lugar
+perso=xd("steven", "paredes", "quito")
